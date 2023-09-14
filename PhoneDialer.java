e9b34c7f09dc4ddfd2acc2af954ee3aa8b5bbabe
Jstowers1 <Lunkman728@gmail.com>
2023-09-14 12:03:43 -0500

    Added phoneDialer

---
+++ PhoneDialer.java
@@ -0,0 +1,16 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class PhoneDialer {
+    public PhoneDialer() {
+        JFrame Dialer = new JFrame();
+        Dialer.setSize(200, 250);
+        
+        Dialer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
